5267a8382f68416111e808c7a98b9c91cb556e1b
* <joelzehring.gmail.com>
2019-12-13 13:28:31 -0700

    logic for responding to user guesses, complete with regular expression tests.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,10 +1,18 @@
 // Wheel of Fortune style guessing game. Guess the letters of the word.
 
-// Wheel of Fortune style guessing game. Guess the letters of the word.
+// Set the target word to be guessed.
 
 var targetWord = "hello";
 
+// Represent the user's progress as an array of booleans, one for each letter in the target word. False for letters that haven't been guessed yet, true for letters that have already been guessed.
+
 var userProgress = [false,false,false,false,false];
+
+// Initiate an array to store letters the user has guessed.
+
+var userGuessList = [];
+
+// Display the word with un-guessed letters hidden
 
 function displayWord(arr) {
   var result = [];
@@ -19,5 +27,30 @@ function displayWord(arr) {
   
   return result.join(" ");
 }
+
+// handle user's guesses in alerts
+
+var nextGuess = prompt("Guess a letter: " + displayWord(userProgress));
+
+if (/[A-z]/.test(nextGuess) && userGuessList.indexOf(nextGuess) >= 0) {
+    alert("That letter has already been suggested. Please try again.");
+} else if (/[A-z]/.test(nextGuess) && targetWord.indexOf(nextGuess) >= 0) {
+    alert("You're right!");
+} else if (/[A-z]/.test(nextGuess) && targetWord.indexOf(nextGuess) < 0) {
+  alert("That letter doesn't appear in the word.");
+}
+  
+/*
+
+document.onkeyup = function(event)
+  var userGuess = event.key;
+  if (if userGuess == /[A-z]/ && userGuessList.indexOf(userGuess) {
+    console.log("That letter has already been suggested. Please try again.")
+  } else if (userGuess == /[A-z]/ && targetWord.indexOf(userGuess)) {
+    console.log("You're right!");
+  }
+}
+
+*/
 
 console.log(displayWord(userProgress));
